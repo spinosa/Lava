e594a86ff02ccd3d4e5fa948548e79fedd7ee5f1
Dan Spinosa <spinosa@gmail.com>
2010-10-27 17:29:16 -0400

    documentation and slight improvements to transform

--- jquery.lava.js
+++ jquery.lava.js
@@ -29,6 +29,7 @@
 		conf: {  
 			load: true,
 			perspective_distance: '700px',
+			transform_t: '.75s',
 			bezier_x1: 0.1,
 			bezier_y1: 0.9,
 			bezier_x2: 0.1,
@@ -43,7 +44,7 @@
 		var self = this,
 			elements = container.children(),
 			width = elements.width(),
-			currentCenter = Math.floor(elements.length / 2);
+			center = currentCenter = Math.floor(elements.length / 2);
 			
 		//API methods
 		$.extend(self, {
@@ -59,10 +60,10 @@
 				elements.css({'position': 'absolute', 
 											'left' : '0px', 
 											'bottom': '-100px',
-											'-webkit-transition': '-webkit-transform 1s cubic-bezier('+conf.bezier_x1+', '+conf.bezier_y1+', '+conf.bezier_x2+', '+conf.bezier_y2+')'});
+											'-webkit-transition': '-webkit-transform '+conf.transform_t+' cubic-bezier('+conf.bezier_x1+', '+conf.bezier_y1+', '+conf.bezier_x2+', '+conf.bezier_y2+')'});
 
 				//initialization: start with middle element centered
-				self.centerElementAt(currentCenter);
+				self.center();
 				
 				if( conf.bind_arrows && typeof jQuery.hotkeys == "object" ){
 					$(document).bind('keyup', 'right', function(){ self.slideRight() });
@@ -77,9 +78,9 @@
 				return self.centerElementAt(currentCenter + x);
 			},
 			
-			slideLeft: function(){ return self.move(1); },
+			slideLeft: function(){ return self.move(-1); },
 			
-			slideRight: function(){ return self.move(-1); },
+			slideRight: function(){ return self.move(1); },
 			
 			centerElementAt: function(index){ 
 				currentCenter = index;
@@ -87,7 +88,9 @@
 				if( currentCenter >= elements.length){ currentCenter = elements.length - 1; }
 				$.lava.lavaflow.centerElement(elements, currentCenter, width);
 				return self;
-			}
+			},
+			
+			center: function(){ return self.centerElementAt(center); }
 			
 		});
 			
